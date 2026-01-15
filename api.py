200662b2c92e2d5110b3a685bd14962b4e746466
ChinnagurvagariHarshavardhanreddy <chinnagurvagariharshavardhanre@gmail.com>
2026-01-15 21:21:50 +0530

    this Basic api demo but has some errors init

--- api.py
+++ api.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
-app=FastAPI(title="this is my fast api Demo")
-@app.get('/{demo}')
-def hello(demo):
-    return f"hello{demo}"
-@app.post('/hesa/hesa')
-async def hesa(user):
-    return user.name
+from pydantic import BaseModel
+
+db=[]
+
+app=FastAPI()
+
+class Student(BaseModel):
+    id : int
+    name : str
+    age : int
+
+@app.get('/item')
+def show():
+    return db
+
+@app.post('/item')
+def create(student:Student):
+    db.append(student)
+    return f'update succefully'
+
+@app.put('/item')
+def update(student:Student):
+    updatedata = student
+    if db[student.id] == student.id:
+        db[student.id] = updatedata
+        return ' update successfully'
+    return 'not there'
+
+@app.delete('/item')
+def delete(student:Student):
+    if db[student.id] == student.id:
+        del db[student.id]
+        return db
+    return 'not there'
+
+@app.get('/item/{id}')
+def take(id):
+    return db[id]
